36bc4af18c2dd04228a62d36aa7338cb20348ac6
Morgan Oleary <morganleeoleary@gmail.com>
2024-06-02 20:54:02 +0100

    Added products app with category and product models

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('products/', include('products.urls')),
 ]
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    categories = models.ManyToManyField('Category')
+    product_id = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    dimensions_height = models.CharField(max_length=100, default='0')
+    dimensions_width = models.CharField(max_length=100, default='0')
+    dimensions_depth = models.CharField(max_length=100, default='0')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
