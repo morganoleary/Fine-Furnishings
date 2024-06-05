5c292d9fd22ac24fe4f18140295572413106341b
Morgan Oleary <morganleeoleary@gmail.com>
2024-06-05 20:08:27 +0100

    Implemented product search bar functionality

--- products/views.py
+++ products/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
-# Create your views here.
+# The all_products view code was implemented with the help of the 
+# Boutique Ado project walkthrough to implement the search bar's functionality
 def all_products(request):
     """ A view to return the all products """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter your search request.")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(categories__name__icontains=query)
+            products = products.filter(queries).distinct()
+
+            if not products.exists():
+                messages.info(request, f'No products found for "{query}"')
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
