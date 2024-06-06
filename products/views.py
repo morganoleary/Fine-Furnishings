418e538b991a2774439405601a1e5d92fb045469
Morgan Oleary <morganleeoleary@gmail.com>
2024-06-06 22:01:16 +0100

    Added category dropdown filters to navbar menu

--- products/views.py
+++ products/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # The all_products view code was implemented with the help of the 
 # Boutique Ado project walkthrough to implement the search bar's functionality
+# and category filtering
 def all_products(request):
     """ A view to render all products """
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -27,6 +34,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'selected_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
