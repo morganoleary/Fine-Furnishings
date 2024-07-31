df3c939842ec6f6ac4f2cd48fdd0fe2efe4e3abf
Morgan Oleary <morganleeoleary@gmail.com>
2024-07-31 20:43:27 +0100

    Update debug in settings for deployment debugging

--- fine_furnishings/settings.py
+++ fine_furnishings/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = ['127.0.0.1', '8000-morganolear-finefurnish-5tmqomrr3vx.ws.codeinstitute-ide.net', 'fine-furnishings-1e63b49cdd1f.herokuapp.com']
 
