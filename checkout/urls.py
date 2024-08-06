fd531727426312f5a76475c130f27a92b83e864d
Morgan Oleary <morganleeoleary@gmail.com>
2024-08-06 13:28:45 +0100

    Added class methods to webhook handler and webhook view

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout/confirmation/<str:order_number>/', views.order_confirmation, name='order_confirmation'),
+    path('wh/', webhook, name='webhook'),
 ]
--- fine_furnishings/settings.py
+++ fine_furnishings/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '8000-morganolear-finefurnish-5tmqomrr3vx.ws.codeinstitute-ide.net', 'fine-furnishings-1e63b49cdd1f.herokuapp.com']
 
@@ -198,6 +198,7 @@ STANDARD_DELIVERY_FEE = 30
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Email settings
 if 'DEVELOPMENT' in os.environ:
